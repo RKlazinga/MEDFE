af0ed2eed0b865e3156ab458aa697ebef5548f36
RKlazinga <r.f.klazinga@student.tudelft.nl>
2021-03-16 14:45:02 +0100

    Fix structure enough to let it display on tensorboard

--- network.py
+++ network.py
@@ -1,49 +1,63 @@
 from torch import nn
 import torch
-from torch.utils import tensorboard
+from torch.utils import tensorboard, data
+from PIL import Image
+import numpy as np
+
 
 class MEDFE(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(4, 64, (4, 4), stride=2)
-        self.conv2 = nn.Conv2d(64, 128, (4, 4), stride=2)
-        self.conv3 = nn.Conv2d(128, 256, (4, 4), stride=2)
-        self.conv4 = nn.Conv2d(256, 512, (4, 4), stride=2)
-        self.conv5 = nn.Conv2d(512, 512, (4, 4), stride=2)
-        self.conv6 = nn.Conv2d(512, 512, (4, 4), stride=2)
+        self.conv1 = nn.Conv2d(4, 64, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv2 = nn.Conv2d(64, 128, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv3 = nn.Conv2d(128, 256, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(256, 512, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv5 = nn.Conv2d(512, 512, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.conv6 = nn.Conv2d(512, 512, (4, 4), stride=(2, 2), padding=(1, 1))
 
         # TODO res block
 
-        self.deconv5 = nn.ConvTranspose2d(1024, 512, (4, 4), stride=2)
-        self.deconv4 = nn.ConvTranspose2d(1024, 256, (4, 4), stride=2)
-        self.deconv3 = nn.ConvTranspose2d(512, 128, (4, 4), stride=2)
-        self.deconv2 = nn.ConvTranspose2d(256, 64, (4, 4), stride=2)
-        self.deconv1 = nn.ConvTranspose2d(128, 4, (4, 4), stride=2)
+        self.deconv5 = nn.ConvTranspose2d(1024, 512, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.deconv4 = nn.ConvTranspose2d(1024, 256, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.deconv3 = nn.ConvTranspose2d(512, 128, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.deconv2 = nn.ConvTranspose2d(256, 64, (4, 4), stride=(2, 2), padding=(1, 1))
+        self.deconv1 = nn.ConvTranspose2d(128, 4, (4, 4), stride=(2, 2), padding=(1, 1))
 
     def forward(self, x):
         x1 = self.conv1.forward(x)
-        x2 = self.conv1.forward(x1)
-        x3 = self.conv1.forward(x2)
-        x4 = self.conv1.forward(x3)
-        x5 = self.conv1.forward(x4)
-        x6 = self.conv1.forward(x5)
+        x2 = self.conv2.forward(x1)
+        x3 = self.conv3.forward(x2)
+        x4 = self.conv4.forward(x3)
+        x5 = self.conv5.forward(x4)
+        x6 = self.conv6.forward(x5)
 
         #TODO apply res block to x6
         # - output should have same shape as x5
-        x_res = torch.tensor(x6)
+        x_res = torch.tensor(x5)
 
-        y5 = self.deconv5.forward(torch.cat((x_res, x5)))
-        y4 = self.deconv4.forward(torch.cat((y5, x4)))
-        y3 = self.deconv3.forward(torch.cat((y4, x3)))
-        y2 = self.deconv2.forward(torch.cat((y3, x2)))
-        y1 = self.deconv1.forward(torch.cat((y2, x1)))
+        x5c = torch.cat((x_res, x5), dim=1)
+        y5 = self.deconv5.forward(x5c)
+        y4 = self.deconv4.forward(torch.cat((y5, x4), dim=1))
+        y3 = self.deconv3.forward(torch.cat((y4, x3), dim=1))
+        y2 = self.deconv2.forward(torch.cat((y3, x2), dim=1))
+        y1 = self.deconv1.forward(torch.cat((y2, x1), dim=1))
+        return y1
 
 
 def main():
+    im = torch.tensor(np.array(Image.open("trial_image.png").convert("RGB")))
+    im = torch.movedim(im, 2, 0)
+    mask = torch.reshape(torch.tensor(np.array(Image.open("30000.png").convert("L"))), (256, 256, 1))
+    mask = torch.movedim(mask, 2, 0)
+
+    x = torch.cat((im, mask), dim=0).float()
+    train_loader = data.DataLoader([x], batch_size=1, shuffle=True, num_workers=1)
+
     medfe = MEDFE()
     writer = tensorboard.SummaryWriter("tensorboard_logs")
-    writer.add_graph(medfe)
+
+    writer.add_graph(medfe, next(iter(train_loader)))
     writer.close()
 
 
