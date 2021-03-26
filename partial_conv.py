cb9dcde4ec7ec8603a25208b1940b95ef00dbdd8
Luc Everse <luc@wukl.net>
2021-03-26 09:41:01 +0100

    Fix PartialConv2d fighting with the tracer

--- partial_conv.py
+++ partial_conv.py
@@ -50,29 +50,29 @@ class PartialConv2d(nn.Conv2d):
 
     def forward(self, input, mask_in=None):
         assert len(input.shape) == 4
-        if mask_in is not None or self.last_size != tuple(input.shape):
-            self.last_size = tuple(input.shape)
 
-            with torch.no_grad():
-                if self.weight_maskUpdater.type() != input.type():
-                    self.weight_maskUpdater = self.weight_maskUpdater.to(input)
+        self.last_size = tuple(input.shape)
 
-                if mask_in is None:
-                    # if mask is not provided, create a mask
-                    if self.multi_channel:
-                        mask = torch.ones(input.data.shape[0], input.data.shape[1], input.data.shape[2], input.data.shape[3]).to(input)
-                    else:
-                        mask = torch.ones(1, 1, input.data.shape[2], input.data.shape[3]).to(input)
+        with torch.no_grad():
+            if self.weight_maskUpdater.type() != input.type():
+                self.weight_maskUpdater = self.weight_maskUpdater.to(input)
+
+            if mask_in is None:
+                # if mask is not provided, create a mask
+                if self.multi_channel:
+                    mask = torch.ones(input.data.shape[0], input.data.shape[1], input.data.shape[2], input.data.shape[3]).to(input)
                 else:
-                    mask = mask_in
+                    mask = torch.ones(1, 1, input.data.shape[2], input.data.shape[3]).to(input)
+            else:
+                mask = mask_in
 
-                self.update_mask = F.conv2d(mask, self.weight_maskUpdater, bias=None, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=1)
+            self.update_mask = F.conv2d(mask, self.weight_maskUpdater, bias=None, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=1)
 
-                # for mixed precision training, change 1e-8 to 1e-6
-                self.mask_ratio = self.slide_winsize/(self.update_mask + 1e-8)
-                # self.mask_ratio = torch.max(self.update_mask)/(self.update_mask + 1e-8)
-                self.update_mask = torch.clamp(self.update_mask, 0, 1)
-                self.mask_ratio = torch.mul(self.mask_ratio, self.update_mask)
+            # for mixed precision training, change 1e-8 to 1e-6
+            self.mask_ratio = self.slide_winsize/(self.update_mask + 1e-8)
+            # self.mask_ratio = torch.max(self.update_mask)/(self.update_mask + 1e-8)
+            self.update_mask = torch.clamp(self.update_mask, 0, 1)
+            self.mask_ratio = torch.mul(self.mask_ratio, self.update_mask)
 
 
         raw_out = super(PartialConv2d, self).forward(torch.mul(input, mask) if mask_in is not None else input)
