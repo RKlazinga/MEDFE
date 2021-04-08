Generalise activation and normalisation, fix multiple issues

bpa.py
@@ -63,7 +63,7 @@ class Bpa(nn.Module):
         self.range_step = RangeStep()
         self.spatial_step = SpatialStep(input_shape)
 
-        self.combine_conv = nn.Conv2d(1024, 512, (1, 1))
+        self.combine_conv = nn.Conv2d(2 * input_shape[1], input_shape[1], (1, 1))
 
     def forward(self, x):
         ranged = self.range_step(x)

branch.py
@@ -21,24 +21,68 @@ class Branch(nn.Module):
         self.input_size = input_size
 
         self.stream7_1 = PartialConv2d(self.input_size, self.input_size, kernel_size=(7, 7), padding=(3, 3))
+        self.relu7_1 = nn.ReLU()
+        self.batch_norm7_1 = nn.BatchNorm2d(self.input_size)
+
         self.stream7_2 = PartialConv2d(self.input_size, self.input_size, kernel_size=(7, 7), padding=(3, 3))
+        self.relu7_2 = nn.ReLU()
+        self.batch_norm7_2 = nn.BatchNorm2d(self.input_size)
+
         self.stream7_3 = PartialConv2d(self.input_size, self.input_size, kernel_size=(7, 7), padding=(3, 3))
+        self.relu7_3 = nn.ReLU()
+        self.batch_norm7_3 = nn.BatchNorm2d(self.input_size)
+
         self.stream7_4 = PartialConv2d(self.input_size, self.input_size, kernel_size=(7, 7), padding=(3, 3))
+        self.relu7_4 = nn.ReLU()
+        self.batch_norm7_4 = nn.BatchNorm2d(self.input_size)
+
         self.stream7_5 = PartialConv2d(self.input_size, self.input_size, kernel_size=(7, 7), padding=(3, 3))
+        self.relu7_5 = nn.ReLU()
+        self.batch_norm7_5 = nn.BatchNorm2d(self.input_size)
 
         self.stream5_1 = PartialConv2d(self.input_size, self.input_size, kernel_size=(5, 5), padding=(2, 2))
+        self.relu5_1 = nn.ReLU()
+        self.batch_norm5_1 = nn.BatchNorm2d(self.input_size)
+
         self.stream5_2 = PartialConv2d(self.input_size, self.input_size, kernel_size=(5, 5), padding=(2, 2))
+        self.relu5_2 = nn.ReLU()
+        self.batch_norm5_2 = nn.BatchNorm2d(self.input_size)
+
         self.stream5_3 = PartialConv2d(self.input_size, self.input_size, kernel_size=(5, 5), padding=(2, 2))
+        self.relu5_3 = nn.ReLU()
+        self.batch_norm5_3 = nn.BatchNorm2d(self.input_size)
+
         self.stream5_4 = PartialConv2d(self.input_size, self.input_size, kernel_size=(5, 5), padding=(2, 2))
+        self.relu5_4 = nn.ReLU()
+        self.batch_norm5_4 = nn.BatchNorm2d(self.input_size)
+
         self.stream5_5 = PartialConv2d(self.input_size, self.input_size, kernel_size=(5, 5), padding=(2, 2))
+        self.relu5_5 = nn.ReLU()
+        self.batch_norm5_5 = nn.BatchNorm2d(self.input_size)
 
         self.stream3_1 = PartialConv2d(self.input_size, self.input_size, kernel_size=(3, 3), padding=(1, 1))
+        self.relu3_1 = nn.ReLU()
+        self.batch_norm3_1 = nn.BatchNorm2d(self.input_size)
+
         self.stream3_2 = PartialConv2d(self.input_size, self.input_size, kernel_size=(3, 3), padding=(1, 1))
+        self.relu3_2 = nn.ReLU()
+        self.batch_norm3_2 = nn.BatchNorm2d(self.input_size)
+
         self.stream3_3 = PartialConv2d(self.input_size, self.input_size, kernel_size=(3, 3), padding=(1, 1))
+        self.relu3_3 = nn.ReLU()
+        self.batch_norm3_3 = nn.BatchNorm2d(self.input_size)
+
         self.stream3_4 = PartialConv2d(self.input_size, self.input_size, kernel_size=(3, 3), padding=(1, 1))
+        self.relu3_4 = nn.ReLU()
+        self.batch_norm3_4 = nn.BatchNorm2d(self.input_size)
+
         self.stream3_5 = PartialConv2d(self.input_size, self.input_size, kernel_size=(3, 3), padding=(1, 1))
+        self.relu3_5 = nn.ReLU()
+        self.batch_norm3_5 = nn.BatchNorm2d(self.input_size)
 
         self.combining_conv = nn.Conv2d(3 * self.input_size, self.input_size, kernel_size=(1, 1))
+        self.combine_relu = nn.ReLU()
+        self.combine_batch_norm = nn.BatchNorm2d(self.input_size)
 
     def set_mask(self, mask):
         n = mask.shape[0]
@@ -46,22 +90,51 @@ class Branch(nn.Module):
 
     def forward(self, f):
         stream_7 = self.stream7_1(f, mask_in=self.mask)
+        stream_7 = self.batch_norm7_1(self.relu7_1(stream_7))
+
         stream_7 = self.stream7_2(stream_7, mask_in=self.mask)
+        stream_7 = self.batch_norm7_2(self.relu7_2(stream_7))
+
         stream_7 = self.stream7_3(stream_7, mask_in=self.mask)
+        stream_7 = self.batch_norm7_3(self.relu7_3(stream_7))
+
         stream_7 = self.stream7_4(stream_7, mask_in=self.mask)
+        stream_7 = self.batch_norm7_4(self.relu7_4(stream_7))
+
         stream_7 = self.stream7_5(stream_7, mask_in=self.mask)
+        stream_7 = self.batch_norm7_5(self.relu7_5(stream_7))
 
         stream_5 = self.stream5_1(f, mask_in=self.mask)
+        stream_5 = self.batch_norm5_1(self.relu5_1(stream_5))
+
         stream_5 = self.stream5_2(stream_5, mask_in=self.mask)
+        stream_5 = self.batch_norm5_2(self.relu5_2(stream_5))
+
         stream_5 = self.stream5_3(stream_5, mask_in=self.mask)
+        stream_5 = self.batch_norm5_3(self.relu5_3(stream_5))
+
         stream_5 = self.stream5_4(stream_5, mask_in=self.mask)
+        stream_5 = self.batch_norm5_4(self.relu5_4(stream_5))
+
         stream_5 = self.stream5_5(stream_5, mask_in=self.mask)
+        stream_5 = self.batch_norm5_5(self.relu5_5(stream_5))
 
         stream_3 = self.stream3_1(f, mask_in=self.mask)
+        stream_3 = self.batch_norm3_1(self.relu3_1(stream_3))
+
         stream_3 = self.stream3_2(stream_3, mask_in=self.mask)
+        stream_3 = self.batch_norm3_2(self.relu3_2(stream_3))
+
         stream_3 = self.stream3_3(stream_3, mask_in=self.mask)
+        stream_3 = self.batch_norm3_3(self.relu3_3(stream_3))
+
         stream_3 = self.stream3_4(stream_3, mask_in=self.mask)
+        stream_3 = self.batch_norm3_4(self.relu3_4(stream_3))
+
         stream_3 = self.stream3_5(stream_3, mask_in=self.mask)
+        stream_3 = self.batch_norm3_5(self.relu3_5(stream_3))
 
         concat = torch.cat((stream_3, stream_5, stream_7), dim=1)
-        return self.combining_conv(concat)
+        combined = self.combining_conv(concat)
+
+        return self.combine_batch_norm(self.combine_relu(combined))

loss.py
@@ -95,13 +95,13 @@ class StylePerceptualLoss(nn.Module):
                 activation_gt[self.layer_name_mapping[name]] = i_gt
                 activation_out[self.layer_name_mapping[name]] = i_out
 
-        # TODO divide by layer necessary when using reduction="mean" ???
         percept_loss = 0
-        percept_loss += self.percept_loss1(activation_gt['relu1_1'], activation_out['relu1_1']) / 5  # 64
-        percept_loss += self.percept_loss2(activation_gt['relu2_1'], activation_out['relu2_1']) / 5  # 128
-        percept_loss += self.percept_loss3(activation_gt['relu3_1'], activation_out['relu3_1']) / 5  # 256
-        percept_loss += self.percept_loss4(activation_gt['relu4_1'], activation_out['relu4_1']) / 5  # 512
-        percept_loss += self.percept_loss5(activation_gt['relu5_1'], activation_out['relu5_1']) / 5  # 512
+        percept_loss += self.percept_loss1(activation_gt['relu1_1'], activation_out['relu1_1'])
+        percept_loss += self.percept_loss2(activation_gt['relu2_1'], activation_out['relu2_1'])
+        percept_loss += self.percept_loss3(activation_gt['relu3_1'], activation_out['relu3_1'])
+        percept_loss += self.percept_loss4(activation_gt['relu4_1'], activation_out['relu4_1'])
+        percept_loss += self.percept_loss5(activation_gt['relu5_1'], activation_out['relu5_1'])
+        percept_loss /= 5
 
         gram_gt = {l: self.gram_matrix(x) for l, x in activation_gt.items()}
         gram_out = {l: self.gram_matrix(x) for l, x in activation_out.items()}
@@ -112,6 +112,7 @@ class StylePerceptualLoss(nn.Module):
         style_loss += self.style_loss3(gram_gt['relu3_1'], gram_out['relu3_1'])
         style_loss += self.style_loss4(gram_gt['relu4_1'], gram_out['relu4_1'])
         style_loss += self.style_loss5(gram_gt['relu5_1'], gram_out['relu5_1'])
+        style_loss /= 5
 
         return percept_loss, style_loss
 

main.py
@@ -6,7 +6,7 @@ from torch.nn.functional import interpolate
 from torch.utils import tensorboard, data
 
 from loss import TotalLoss
-from network import MEDFE
+from network.network import MEDFE
 from dataset import CustomDataset
 from torchvision import transforms
 import PIL
@@ -27,9 +27,10 @@ def main(args):
     img_folder = "data/celeba/img_align_celeba"
     train_size = args.train_size  # celeba dataset is 202k images large
     training_set = CustomDataset(img_folder, img_folder+"_tsmooth", train_size)
-    train_loader = data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True, num_workers=1)
+    train_loader = data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
 
-    model = MEDFE().to(device)
+    model = MEDFE(branch_channels=64,
+                  channels=8).to(device)
     optimiser = optim.Adam(model.parameters(), lr=args.learning_rate)
     criterion = TotalLoss()
 
@@ -70,7 +71,7 @@ def main(args):
                     unmasked = first_in_batch
                     if first_in_batch.shape[1] == 4:
                         unmasked = first_in_batch.split([3, 1], dim=1)[0]
-                    return unmasked.reshape(3, x, y)
+                    return torch.clamp(unmasked.reshape(3, x, y), 0, 1)
 
                 im_masked_image = to_im_shape(batch['masked_image'])
                 im_gt = to_im_shape(gt256)
@@ -78,7 +79,7 @@ def main(args):
                 if model.struct_branch_img is not None:
                     im_st = to_im_shape(model.struct_branch_img, 32, 32)
                     im_te = to_im_shape(model.tex_branch_img, 32, 32)
-                im_out = torch.clamp(out[0], 0, 1)
+                im_out = to_im_shape(out)
 
                 im = PIL.Image.new('RGB', (3 * 256, 2 * 256))
                 im.paste(to_pil(im_masked_image), (0, 0))
@@ -93,7 +94,7 @@ def main(args):
                 iml = tk.Label(win, image=tkimg)
                 iml.pack()
 
-                ll = tk.Label(win, text=f"| || || |_: {single_loss.item()}")
+                ll = tk.Label(win, text=', '.join([f"{loss_name}:{round(float(value),2)}" for loss_name, value in criterion.last_loss.items()]))
                 ll.pack()
                 win.update()
                 iml.pack_forget()
@@ -109,8 +110,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Train the image inpainting network')
 
-    parser.add_argument('--train-size', default=100, type=int, help='the number of images to train with')
-    parser.add_argument('--batch-size', default=25, type=int, help='the number of images to train with in a single batch')
+    parser.add_argument('--train-size', default=5000, type=int, help='the number of images to train with')
+    parser.add_argument('--batch-size', default=50, type=int, help='the number of images to train with in a single batch')
     parser.add_argument('--learning-rate', default=1e-3, type=float, help='the learning rate')
     parser.add_argument('--cuda', action='store_true', help='run with CUDA')
     parser.add_argument('--output-intermediates', action='store_true', help='show intermediate results in a GUI window')